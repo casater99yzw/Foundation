f901cc49d4748cbf609dfab62272580f1dc37c69
??? <ylqtfosorcimylq@hotmail.com>
2017-06-23 15:48:54 +0800

    try commit from sub-module location.

---
+++ Foundation/GeometryFoundation.h
@@ -0,0 +1,65 @@
+#pragma once
+#include "BasicType.h"
+#include <array>
+
+namespace X
+{
+	template <typename T, uint32_t Dimension>
+	struct Size
+	{
+		std::array<T, Dimension> data;
+		explicit constexpr Size(std::array<T, Dimension> const& data) noexcept
+			: data(data)
+		{
+		}
+		explicit constexpr Size(T const& x) noexcept
+		{
+			static_assert(Dimension == 1, "");
+			data[0] = x;
+		}
+		explicit constexpr Size(T const& x, T const& y) noexcept
+		{
+			static_assert(Dimension == 2, "");
+			data[0] = x;
+			data[1] = y;
+		}
+		explicit constexpr Size(T const& x, T const& y, T const& z) noexcept
+		{
+			static_assert(Dimension == 3, "");
+			data[0] = x;
+			data[1] = y;
+			data[2] = z;
+		}
+
+		constexpr T const& X() const noexcept
+		{
+			static_assert(Dimension >= 1, "");
+			return data[0];
+		}
+		constexpr T const& Y() const noexcept
+		{
+			static_assert(Dimension >= 2, "");
+			return data[1];
+		}
+		constexpr T const& Z() const noexcept
+		{
+			static_assert(Dimension >= 3, "");
+
+			return data[2];
+		}
+
+		T& operator [](uint32_t index) noexcept
+		{
+			assert(index < Dimension);
+			return data[index];
+		}
+		constexpr T const& operator [](uint32_t index) const noexcept
+		{
+			assert(index < Dimension);
+			return data[index];
+		}
+	};
+
+	using Size2UI = Size<uint32, 2>;
+	using Size2SI = Size<sint32, 2>;
+}
